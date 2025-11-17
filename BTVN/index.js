a400ecb2c319a641ba04067c1819d83909e45476
NTD2k11 <tranquangle00@gmail.com>
2025-11-17 20:56:10 +0700

    btvn

---
+++ BTVN/index.js
@@ -0,0 +1,9 @@
+let box = document.createElement('div');
+box.className = 'box';
+box.innerHTML = `
+        <img src="https://static.kfcvietnam.com.vn/images/category/lg/KHUYEN%20MAI.jpg?v=Yg2Nng" alt="" class="img_in_box">
+        <h3>Ưu Đãi ></h3>
+    
+`;
+document.querySelector('.off').appendChild(box)
+
